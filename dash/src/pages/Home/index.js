17e29e81cbb40de0dd712bde0fd713418f7926e8
Fabi Rodrigues <frontfabi@gmail.com>
2022-11-27 13:54:42 -0300

    [ADD] Front-End Files

--- dash/src/pages/Home/index.js
+++ dash/src/pages/Home/index.js
@@ -1,28 +1,20 @@
 import { SearchInput } from './../../components/SearchInput'
 import { StyledHome } from './styles';
-import { getByName, getFromRiot } from '../../services/Summoners'
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../contexts';
 //import {} from '../src/index.js'
 
 export function Home() {
-  const servers = [
-    { label: 'BR1', value: 'br1' },
-    { label: 'EUN1', value: 'eun1' },
-    { label: 'EUW1', value: 'euw1' },
-    { label: 'JP1', value: 'jp1' },
-    { label: 'KR', value: 'kr' },
-    { label: 'LA1', value: 'la1' },
-    { label: 'LA2', value: 'la2' },
-    { label: 'NA1', value: 'NA2' },
-    { label: 'OC1', value: 'oc1' },
-    { label: 'TR1', value: 'tr1' },
-    { label: 'RU', value: 'ru' },
-  ]
-
-  
-
+  const { serverList, login, loginData } = useContext(AuthContext)
+  const [profileImg, setProfileImg] = useState(null)
+  useEffect(() => {
+    setProfileImg(loginData.profileIcon)
+  }, [loginData.profileIcon])
+  console.log(profileImg)
   return (
     <StyledHome>
-      <SearchInput servers={servers} label="Digite o seu Riot ID" searchAction={getFromRiot} />
+      <img src={profileImg} alt="" />
+      <SearchInput servers={serverList} label="Digite o seu Riot ID" searchAction={login} />
     </StyledHome>
   );
 }
